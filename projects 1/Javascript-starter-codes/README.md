aaf4c0f93e66dff9a2a568d961a1e84dba3976c5
ashtyui <90778407+ashtyui@users.noreply.github.com>
2022-06-11 06:51:25 +0300

    Add files via upload

---
+++ projects 1/Javascript-starter-codes/README.md
@@ -0,0 +1,2 @@
+# Javascript-starter-codes
+hello this is my site I like it you can change it if you want.
---
+++ projects 1/Javascript-starter-codes/index.js
@@ -0,0 +1,3 @@
+
+    alert('Hello,I am A-453 i will show you this site to adventure.');
+    alert('Do you like Miles I like miles here is a site');
